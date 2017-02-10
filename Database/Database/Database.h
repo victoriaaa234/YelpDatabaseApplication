8e2043a990e16c1ef7190ad3b60439e639dab052
Yerania Hernandez <Yerania Hernandez>
2017-02-09 20:29:30 -0600

    #2 Added Database API Endpoints

--- Database/Database/Database.h
+++ Database/Database/Database.h
@@ -6,6 +6,10 @@
 #define DATABASE_API __declspec(dllimport)   
 #endif  
 
+#include <iostream>
+#include <vector>
+#include <map>
+
 namespace Database
 {
 	// This class is exported from the Database.dll  
@@ -14,9 +18,17 @@ namespace Database
 		class Query
 		{
 		public:
+
 		};
 	public:
-		
+		DATABASE_API Database();
+		DATABASE_API bool addTable(Table tbl, std::string name);
+		DATABASE_API bool dropTable(std::string tblName);
+		DATABASE_API std::vector<std::string> listTables();
+		DATABASE_API std::map<std::string, Table> getTables();
+		DATABASE_API Table query(std::string select, std::string from, std::string where);
+
+
 		// Returns a + b  
 		//static MATHLIBRARY_API double Add(double a, double b);
 
